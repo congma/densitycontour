14655476cd1453c6444374b06b0585f28ae0f7c4
Cong Ma <cma@pmo.ac.cn>
2014-06-15 15:41:45 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,61 @@
+# densitycontour
+
+`densitycontour` -- draw density contours from sample points.
+
+
+## USAGE
+
+`densitycontour` is a Python module that helps with creating contour plots from
+a sample of points.  It is useful for visualizing the output of Markov Chain
+Monte Carlo (MCMC) sampling.
+
+Typical usage is like follows:
+
+```python
+import pylab	# Import matplotlib environment.
+import densitycontour
+
+# Create scatter-data and rasterized image objects.
+# x_array and y_array are "raw" inputs.
+sample_data = densitycontour.ScatterData(x_array, y_array)
+
+# The 32x32 raster array used for plotting.
+raster = sample_data.rasterize(32, 32)
+
+# Use the ZoomedContourVisualizer post-processor on the raster array.
+contours = densitycontour.ZoomedContourVisualizer(raster, mode="nearest")
+
+# Plot the contours for confidence levels 50% and 90% respectively,
+# using default settings.
+contours.plot((0.5, 0.9))
+
+# Show the figure.
+pylab.show()
+```
+
+You can run the module as a Python script to see the test diagrams.
+
+
+## DEPENDENCY
+
+`densitycontour` requires the [`numpy`][numpy], [`scipy`][scipy],
+and [`matplotlib`][matplotlib] packages.
+
+
+## COPYRIGHT
+
+Copyright © 2014 Cong Ma.  License BSD: See the COPYING file.
+
+This is free software: you are free to change and redistribute it.  
+There is NO WARRANTY, to the extent permitted by law.
+
+
+## AVAILABILITY
+
+Available from [https://github.com/congma/densitycontour][githubrepo].
+
+
+[githubrepo]: https://github.com/congma/densitycontour "GitHub repository page for densitycontour"
+[numpy]: http://www.numpy.org/ "NumPy"
+[scipy]: http://www.scipy.org/scipylib/index.html "SciPy library"
+[matplotlib]: http://matplotlib.org/ "matplotlib"
---
+++ densitycontour.py
@@ -0,0 +1,327 @@
+"""densitycontour -- draw density contours from sample points."""
+# Disable Pylint false positives due to numpy/scipy's over-smart import
+# pylint: disable=E1101
+import weakref
+import numpy
+import scipy.optimize
+import scipy.ndimage
+import pylab
+
+
+__all__ = ["ScatterData", "RasterizedData", "ContourVisualizerBase",
+           "NaiveContourVisualizer", "ZoomedContourVisualizer",
+           "MFZoomedContourVisualizer"]
+
+
+def _getkeybysig(nbins_x, nbins_y, clipping):
+    """Calculate raster interning key by the signature of its creation call."""
+    nbx = int(nbins_x)
+    nby = int(nbins_y)
+    if clipping is not None:
+        # Unpack the range parameters and store as Python native type.
+        ranges = tuple(float(x) for x in numpy.asarray(clipping).flat)
+    else:
+        ranges = None
+    return (nbx, nby, ranges)
+
+
+# pylint: disable=C0103
+def _find_confidence_interval(x, pdf, confidence_level):
+    """Function whose root, as the desired image-value level for
+    the input confidence level, is to be found.
+    """
+    return pdf[pdf > x].sum() - confidence_level
+# pylint: enable=C0103
+
+
+def _destroy_dict(what, num, totally):
+    """Destroy up to num dictionary items, unless totally = True, in which case
+    the dictionary is cleared.  Items are destroyed in no particular order.
+
+    Arguments
+    ---------
+    what: Dictionary whose items are to be destroyed.
+    num: Integer, maximal (inclusive) number of items to destroy.  If not
+         given as an integer, will be floor-rounded as one.
+    totally: Boolean flat that, when specified, will cause the entire
+             dictionary to be destroyed.
+
+    Return value
+    ------------
+    The actual number of items destroyed.
+    """
+    assert num >= 0
+    num = int(num)
+    if totally:
+        count = len(what)
+        what.clear()
+    else:
+        count = 0
+        for i in xrange(num):   # pylint: disable=W0612
+            try:
+                what.popitem()
+            except KeyError:    # Nothing left
+                break           # Exit the "for i in xrange()" loop
+            count += 1
+    return count
+
+
+class ScatterData(object):
+    """Class holding reference to the scatter-point data."""
+    def __init__(self, xdata, ydata):
+        """Initialize with x- and y-data.  Both must be same-length
+        (i.e. parallel) numpy arrays.
+        """
+        self.xdata = xdata
+        self.ydata = ydata
+        # Weak dict as memo table of rasterizations.
+        self._rasters = weakref.WeakValueDictionary()
+        return None
+
+    def rasterize(self, nbins_x, nbins_y, clipping=None):
+        """Rasterize self by binning the scattered points.
+
+        Arguments
+        --------
+        nbins_x, nbins_y: Number of bins in the x- and y- dimensions.
+        clipping: Valid ranges of data, points outside of which are considered
+                  outliers and discarded in further processings.
+
+        Return value
+        ------------
+        A RasterizedData instance, possibly a reference to an already-memoized
+        one.
+        """
+        # Produce the memo key by normalizing the input parameters.
+        memokey = _getkeybysig(nbins_x, nbins_y, clipping)
+        if memokey in self._rasters:    # Try consulting the memo table first.
+            return self._rasters[memokey]
+        # Not yet in memo, so create-and-intern it.
+        result = RasterizedData(self, memokey[0], memokey[1],
+                                clipping=clipping)
+        self._rasters[memokey] = result
+        return result
+
+    def forget_rasters(self, num=1, totally=False):
+        """Forget memoized rasters in no particular order.
+
+        Arguments
+        ---------
+        num: Maximal number of rasters to forget.  The actual number of rasters
+        forgotten may be less than num.
+        totally: If True, forget all rasters, ignoring num.
+
+        Return value
+        ------------
+        The number of rasters actually forgotten.
+        """
+        return _destroy_dict(self._rasters, num, totally)
+
+
+class RasterizedData(object):
+    """Class holding the rasterized data."""
+    def __init__(self, other, nbins_x, nbins_y, clipping=None):
+        """Bin the data and create empirical PDF from other.
+
+        Arguments
+        ---------
+        other: The ScatterData instance to which this raster instance will
+               belong.
+        nbins_x, nbins_y: Number of bins in the x- and y- dimensions.
+        clipping: Valid ranges of data, points outside of which are considered
+                  outliers and discarded in further processings.
+        """
+
+        hist, self.xedges, self.yedges = numpy.histogram2d(
+            other.xdata, other.ydata,
+            bins=(nbins_x, nbins_y),
+            range=clipping, normed=True)
+        x_bin_sizes = (self.xedges[1:] -
+                       self.xedges[:-1]).reshape((1, nbins_x))
+        y_bin_sizes = (self.yedges[1:] -
+                       self.yedges[:-1]).reshape((nbins_y, 1))
+        self.pdf = hist * (x_bin_sizes * y_bin_sizes)
+        self.levels = {}
+        return None
+
+    def level_by_confidence(self, confidence_level):
+        """Convert confidence to "image" level value.
+
+        The result is memoized in self.levels during computation, if necessary.
+
+        Arguments
+        ---------
+        confidence_level: the confidence level (0 <= p <= 1).
+
+        Return value
+        ------------
+        The level value of given input.
+        """
+        assert 0.0 <= confidence_level <= 1.0
+        # Use natively typed value as key.
+        conf_native = float(confidence_level)
+        if conf_native in self.levels:   # if already memoized
+            return self.levels[conf_native]
+        res = scipy.optimize.brentq(_find_confidence_interval, 0., 1.,
+                                    args=(self.pdf, confidence_level))
+        self.levels[conf_native] = res
+        return res
+
+    def forget_levels(self, num=1, totally=False):
+        """Forget up to num plotting levels, unless totally = True,
+        in which case all levels are cleared.
+
+        Arguments
+        ---------
+        num: Maximal number of rasters to forget.  The actual number of rasters
+        forgotten may be less than num.
+        totally: If True, forget all rasters, ignoring num.
+
+        Return value
+        ------------
+        The actual levels forgotten.
+        """
+        return _destroy_dict(self.levels, num, totally)
+
+
+# pylint: disable=R0903
+class ContourVisualizerBase(object):
+    """Base class of contour visualizer for rasterized data.
+
+    Not meant to be instantiated directly.
+    """
+    def __init__(self, other, *args, **kwargs):  # pylint: disable=W0613
+        """Initialize visualizer by a RasterizedData instance.
+
+        Arguments
+        ---------
+        other: The RasterizedData instance for which visualization
+               will be done.
+
+        Subclasses will determine the use of *args and **kwargs.
+        """
+        self._xorig = 0.5 * (other.xedges[1:] + other.xedges[:-1])
+        self._yorig = 0.5 * (other.yedges[1:] + other.yedges[:-1])
+        self._zorig = other.pdf.T
+        self.levelfinder = other.level_by_confidence
+        return None
+
+    def plot(self, confidence_levels, axes=None, **contour_kwargs):
+        """Plot as matplotlib contour.
+
+        Arguments
+        ---------
+        confidence_levels: Container of confidence levels
+        (0 <= p <= 1) for which the contours shall be drawn.
+        axes: If given, will be used as the matplotlib "Axes"
+              to which the contour will be added.
+        Keyword arguments: Passed to matplotlib directly, except "levels".
+
+        Return value
+        ------------
+        matplotlib's QuadContourSet object.
+        """
+        imglevels = [self.levelfinder(x) for x in confidence_levels]
+        if axes is None:
+            axes = pylab
+        contour = axes.contour(self._xdat, self._ydat, self._zdat,
+                               levels=imglevels,
+                               origin="lower", **contour_kwargs)
+        return contour
+
+
+class NaiveContourVisualizer(ContourVisualizerBase):
+    """The "naive" contour visualizer.
+
+    This visualizer simply accpets the raster as it is, without any
+    post-processing.
+
+    """
+    def __init__(self, other, *args, **kwargs):
+        super(NaiveContourVisualizer, self).__init__(other, *args, **kwargs)
+        self._xdat = self._xorig
+        self._ydat = self._yorig
+        self._zdat = self._zorig
+        return None
+
+
+class ZoomedContourVisualizer(ContourVisualizerBase):
+    """This visualizer uses interpolated upsampling to provide
+    better-shaped contours.
+    """
+    def __init__(self, other, zoom=3, zoom_prefilter=False, *args, **kwargs):
+        """Initialize a ZoomedContourVisualizer instance.
+
+        Additional arguments
+        --------------------
+        zoom: Zoom factor, default 3.
+        zoom_prefilter: Boolean, whether the raster image should be processed
+                        by the spline filter (scipy.ndimage.spline_filter)
+                        before enlarging.  The filter is a high-pass filter
+                        that is intended to enhance details, but may cause
+                        undesirable artefacts in the processed image.  See
+                        documentation of scipy.ndimage.zoom.
+        *args are delegated to parent class.
+        **kwargs are passed to scipy.ndimage.zoom.
+        """
+        super(ZoomedContourVisualizer,
+              self).__init__(other, *args, **kwargs)
+        self._xdat = scipy.ndimage.zoom(self._xorig, zoom,
+                                        prefilter=zoom_prefilter, **kwargs)
+        self._ydat = scipy.ndimage.zoom(self._yorig, zoom,
+                                        prefilter=zoom_prefilter, **kwargs)
+        self._zdat = scipy.ndimage.zoom(self._zorig, zoom,
+                                        prefilter=zoom_prefilter, **kwargs)
+        return None
+
+
+class MFZoomedContourVisualizer(ZoomedContourVisualizer):
+    """Upsampling visualizer with post-blurring using median filter."""
+    def __init__(self, other, size=4, zoom_prefilter=True, *args, **kwargs):
+        """Initialize a MFZoomedContourVisualizer instance.
+
+        Additional arguments
+        --------------------
+        size: The size of median filter, see documentation of
+              scipy.ndimage.median_filter.  Default 4.
+        *args are delegated to parent class.
+        **kwargs are passed to scipy.ndimage.zoom.
+        """
+        super(MFZoomedContourVisualizer,
+              self).__init__(other, zoom_prefilter=zoom_prefilter,
+                             *args, **kwargs)
+        self._zdat = scipy.ndimage.median_filter(self._zdat,
+                                                 size=size, **kwargs)
+        return None
+# pylint: enable=R0903
+
+
+def _test():
+    """Module self-test."""
+    data_array = numpy.random.multivariate_normal([0, 0],
+                                                  [[1, 0], [0, 1]], 20000)
+    data = ScatterData(data_array[:, 0], data_array[:, 1])
+    raster = data.rasterize(32, 32)
+    vis_backends = (NaiveContourVisualizer, ZoomedContourVisualizer,
+                    MFZoomedContourVisualizer)
+    # pylint: disable=W0612
+    fig, axs = pylab.subplots(1, 3, sharex=True, sharey=True)
+    # pylint: enable=W0612
+    for i in xrange(len(vis_backends)):
+        vbackend = vis_backends[i]
+        sub = axs[i]
+        sub.scatter(data.xdata[::4], data.ydata[::4],  # thinned by 4
+                    alpha=0.1, facecolor="green",
+                    edgecolor="green", marker=".", antialiased=True)
+        cont = vbackend(raster, mode="nearest")
+        cont.plot([0.5, 0.9], axes=sub, colors="black",
+                  linestyles=["-", "--", ":"], antialiased=True, alpha=0.8)
+        sub.set_aspect("equal", adjustable="box-forced", anchor="C")
+        sub.set_ylim((-3.5, 3.5))
+        sub.set_xlim((-3.5, 3.5))
+    pylab.show()
+    return None
+
+
+if __name__ == "__main__":
+    _test()
